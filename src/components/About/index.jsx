9af7e9fbefaad432b230a2f0f641bb7f8f166460
Sujatadevi <55720523+Sujatadevi@users.noreply.github.com>
2020-10-22 12:23:13 -0400

    Lab2JS
    final commit

---
+++ src/components/About/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../shared/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header title={"React Router App"} />
+      <p style={{ paddingLeft: '2em', color: '#2c3e50', marginTop: '0.5em' }}>Travel Enthusiast. I have visited 5 countries in my life. </p>
+    </>
+  );
+}
+
+export default Home;
